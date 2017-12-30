bc84a056a0606139619976bd7c52ec5abc399c07
soyyo5159 <soyyo5159@hotmail.com>
2017-12-29 21:43:24 -0300

    Base de datos

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var express = require('express')
 var app = express()
+const MongoClient = require('mongodb').MongoClient;
 
 var bodyParser = require('body-parser')
+const url = 'mongodb://pp:pp@ds133597.mlab.com:33597/josecito';
+
+const db
+
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+  
+    db = client.db(dbName);
+});
+  
+
 
 
 app.use(bodyParser.json())
@@ -9,9 +22,17 @@ app.use(bodyParser.json())
 app.get('/favicon.ico', function(req, res) {
     res.status(204);
 });
- 
-app.get('/hola', function (req, res) {
-  res.send('Hello World juancitovbbbbbbbbbbbbbbbbbbbbb szd/xx')
+
+app.get('/:nombre', function (req, res) {
+    console.log(req.params);
+    db.collection("mediciones").findOne({nombre:req.params.nombre});
+})
+
+app.post('/:nombre', function (req, res) {
+    let o=req.body;
+    o.nombre=req.params.nombre;
+    console.log(req.params);
+    db.collection("mediciones").insertOne(o);
 })
 
 app.use(function(req,res){
